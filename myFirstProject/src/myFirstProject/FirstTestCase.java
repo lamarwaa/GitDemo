ad7ed9aeba9ff5e6f7895f3369af265db17802ff
marwa <lamarwaa@gmail.com>
2020-01-09 22:57:43 +0100

    first commit

---
+++ myFirstProject/src/myFirstProject/FirstTestCase.java
@@ -0,0 +1,24 @@
+package myFirstProject;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTestCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver" , "C:\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String url = "http://toolsqa.com/automation-practice-table/" ;
+		driver.get(url);
+		
+
+		
+
+	}
+
+}
